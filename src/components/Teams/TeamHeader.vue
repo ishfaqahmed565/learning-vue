81312b38f4046adc5d75ba2e018b0c334af687be
Ahmad Ishfaq Abdullah <ahmedishfaq737@gmail.com>
2023-06-14 23:32:39 +0600

    Update TeamHeader.vue

--- src/components/Teams/TeamHeader.vue
+++ src/components/Teams/TeamHeader.vue
@@ -9,7 +9,7 @@ let team = useTeamStore();
     <div>
       <div class="inline-flex items-center text-3xl relative">
         <h3>
-          <img src="/smiley.png" alt="" class="mr-2" />
+          
           {{ team.name }} Team
           <div
             class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
